aa04bf030a78eb9526ec9e8d5339d3afa48eba6e
Drew <bridgmand@gmail.com>
2021-02-04 14:29:56 -0500

    created database and intserted basic data

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Double Meat');
+INSERT INTO burgers (burger_name, good) VALUES ('Blue Cheese', false);
+INSERT INTO burgers (burger_name, good) VALUES ('With Pickels', false);
