d5ccaa0c6b11a6cbeaf93dea83729279a9ab839b
Drew <bridgmand@gmail.com>
2021-02-04 14:29:09 -0500

    created structure for routes

---
+++ config/orm.js
@@ -0,0 +1,21 @@
+const connection = require('./connection')
+
+const orm = {
+    selectAll() {
+
+    },
+    insert() {
+
+    },
+    updateOne() {
+
+    },
+};
+
+
+
+
+
+
+
+module.exports = orm;
