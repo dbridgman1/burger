9148056bd5450a2a3a2f93c6b09462077b4257c5
Drew <bridgmand@gmail.com>
2021-02-06 09:53:41 -0500

    setup routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,15 +3,26 @@ const router = express.Router();
 const burger = require ('../models/burger');
 
 router.get('/', (req, res) => {
-
+   burger.selectAll((data) => {
+       var burger = {
+           burger_data: data,
+       };
+       res.render('index', burger)
+   })
 });
 
 router.post('/api/burgers', (req,res) => {
-
+    burger.insert(req.body.good, (result) => {
+        console.log(result);
+        res.redirect('/');
+    })
 });
 
 router.put('/api/burgers/:id', (req, res) => {
-
+    burger.updateOne(req.params.id, (result) => {
+        console.log(result);
+        res.sendStatus(200)
+    })
 });
 
 module.exports = router;
