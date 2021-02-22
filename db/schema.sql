10da4ba5ad5c68ebbb19c95b83e49a9e8d99ed99
Drew <bridgmand@gmail.com>
2021-02-22 12:52:57 -0500

    spelling changes and seed files fixes

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,11 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
+DROP DATABASE IF EXISTS burger_db;
+CREATE DATABASE burger_db;
+USE burger_db;
 
 CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT ,
-    name varchar(30) NOT NULL,
-    good BOOLEAN DEFAULT false,
-    PRIMARY KEY(id)
+	id int NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	good BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,9 @@
-USE burgers_db;
+  
+USE burger_db;
 
-INSERT INTO burgers (name) VALUES ('Bacon', true);
-INSERT INTO burgers (name) VALUES ('Cheese', true);
-INSERT INTO burgers (name) VALUES ('Double Meat', true);
-INSERT INTO burgers (name, good) VALUES ('Blue Cheese');
-INSERT INTO burgers (name, good) VALUES ('With Pickels');
+INSERT INTO burgers (name, good) VALUES ('Bacon', true);
+INSERT INTO burgers (name, good) VALUES ('Cheese', true);
+INSERT INTO burgers (name, good) VALUES ('Double Meat', true);
+INSERT INTO burgers (name, good) VALUES ('Blue Cheese', false);
+INSERT INTO burgers (name, good) VALUES ('With Pickels', false);
+INSERT INTO burgers (name, good) VALUES ("Lettuce Tomato Mayo", false);
