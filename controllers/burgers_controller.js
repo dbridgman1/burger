84a26d3d388a8201366b502638eb999e7ce9dcb4
Drew <bridgmand@gmail.com>
2021-02-22 12:51:18 -0500

    fixed routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,39 +1,42 @@
-const express = require('express');
+const express = require("express");
+
 const router = express.Router();
-const burger = require ('../models/burger.js');
 
-router.get('/', (req, res) => {
-   burger.selectAll((data) => {
-       const hbsObject = {
-           burgers: data,
-       };
-       console.log('burger', hbsObject)
-       res.render('index', hbsObject)
-   })
+const burger = require("../models/burger.js");
+
+router.get("/", (req, res) => {
+  burger.all((data) => {
+    const hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
-router.post('/api/burgers', (req,res) => {
-    burger.insert(['name', 'good'], [req.body.name, req.body.good], (result) => {
-        console.log(result);
-        res.json({ id: result.insertID});
-    })
+router.post("/api/burgers", (req, res) => {
+  burger.create(["name", "good"], [req.body.name, req.body.good], (result) => {
+    res.json({ id: result.insertId });
+  });
 });
 
-router.put('/api/burgers/:id', (req, res) => {
-    const condition = `id = ${req.params.id}`;
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = `id = ${req.params.id}`;
 
-    console.log('condition', condition);
-    burger.updateOne(
-        {
-            good: req.body.good,
-        },
-        condition, 
-        (result) => {
-            if (result.changedRows === 0) {
-                return res.status(404).end();
-            }
-            res.status(200).end();
-    })
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      good: req.body.good,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
 });
 
 module.exports = router;
